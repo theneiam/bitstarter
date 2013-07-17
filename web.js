597233c22aeae1f0d2292913b11257c172cd6575
Eugene I. Nezhuta <eugene.nezhuta@gmail.com>
2013-07-17 23:57:09 +0300

    added index.html and modified web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
+var fs      = require('fs');
 var express = require('express');
+var app     = express();
 
-var app = express.createServer(express.logger());
+app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var buffer = fs.readFileSync('index.html');
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
