7cbd8ab7a1d8374b2104c5b48bad9ecd991ab7ec
Eugene I. Nezhuta <eugene.nezhuta@gmail.com>
2013-07-18 00:04:39 +0300

    web.js modified

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
 var fs      = require('fs');
 var express = require('express');
-var app     = express();
 
-app.use(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var buffer = fs.readFileSync('index.html');
@@ -10,7 +9,6 @@ app.get('/', function(request, response) {
 });
 
 var port = process.env.PORT || 5000;
-
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
